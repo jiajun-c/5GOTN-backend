3202b528eb4e60ca1cd9997217dab91712224fa7
jiajun-c <2966623227@qq.com>
2023-03-14 14:09:39 +0800

    grpc demo

--- README.md
+++ README.md
@@ -30,10 +30,26 @@ Out proto file should be put in the pb dictionary.
 
 ## 2. Run this project
 
-Start the docker of db....etc
+Install the grpc requirements for golang and python first [doc link](https://grpc.io/docs/languages/go/quickstart/)
+
+Generate the grpc code for python server and the golang client.
+
+```shell
+make rpc
+
+cd cmd/core/pb
+make rpc
+```
+
+```shell
+python main.cpp
+
+go run ./app
+```
+
 ```shell
 docker-compose up -d
-```
+```e
 
 Start the services
 
--- app/main.go
+++ app/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"otn/pb"
+	"time"
+)
 
+const (
+	defaultName = "world"
+)
+
+func main() {
+	flag.Parse()
+	// 连接到server端，此处禁用安全传输
+	conn, err := grpc.Dial("127.0.0.1:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// 执行RPC调用并打印收到的响应数据
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "hello"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
 }
