24488f271cd597efad2a64dd6dc0e2696e9bab70
jiajun-c <2966623227@qq.com>
2023-03-22 12:07:50 +0800

    add the cors

--- app/main.go
+++ app/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/spf13/viper"
 	"otn/api/router"
 	"otn/config"
+	"otn/middlewares"
 )
 
 func init() {
@@ -13,6 +14,7 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	r.Use(middlewares.Cors())
 	router.Route(r)
 	addr := viper.GetString("api.addr")
 	r.Run(addr)
