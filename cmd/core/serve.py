eab3498c00be1055cce418e28505d4ca8b41f15c
张庙松 <1875912440@qq.com>
2023-04-14 12:31:11 +0800

    update:update the core function using grpc

--- cmd/core/serve.py
+++ cmd/core/serve.py
@@ -1,39 +1,48 @@
-# -*- coding: utf-8 -*-
-# @Time : 2023/3/18 14:27
-# @Author : Zhangmiaosong
+import grpc
+import core_pb2_grpc
+import core_pb2
+import CorrelationAnalysis
+import predict
+import Train
 from concurrent import futures
 import grpc
-import Data_pb2 as data_pb2
-import Data_pb2_grpc as data_pb2_grpc
 
-class DataService(data_pb2_grpc.DataServiceServicer):
-    def SendData(self, request, context):
-        print("Send data successfully!")
-        print(request)
-        data_reply = data_pb2.DataReply()
-        data_list = []
-        # 假设这里要返回一个包含5个Data对象的数组
-        for i in range(1,6):
-            data = data_pb2.DataRequest()
-            # 填充data对象的属性值
-            data.clogid = i
-            data.cneid = i
-            data.clineport = str(i)  # 必须转换为字符串类型
-            data.clocationinfo = f"location_{i}"
-            data.coccurutctime = f"2022-01-01 00:0{i}:00"
-            data.calarmcode = i
-            data.calarmlevel = i
-            data_list.append(data)
-        data_reply.data.extend(data_list)
-        return data_reply
+class CoreServicer(core_pb2_grpc.coreServicer):
+    # 实现Train方法
+    def Train(self, request, context):
+        # 进行训练，并返回结果
+        data = request.file_content
+        all_account, level0, level1, level2, level3 = Train(data)
+        response = core_pb2.TrainResponse(all_account=all_account, level0=level0, level1=level1, level2=level2, level3=level3)
+        return response
+
+    # 实现CorrelationAnalysis方法
+    def CorrelationAnalysis(self, request, context):
+        # 进行相关性分析，并返回结果
+        data = request.file_content
+        state = CorrelationAnalysis(data)
+        response = core_pb2.CorrelationAnalysisResponse(success=state, model_name="model", model_content=b"content", train_name="train", train_content=b"content", loc_name="loc", loc_content=b"content", locToIndexOflocs_name="locToIndexOflocs", locToIndexOflocs_content=b"content")
+        return response
+
+    # 实现Alert方法
+    def Alert(self, request, context):
+        # 进行预测，并返回结果
+        data_request = request.datarequest
+        input_data = []
+        for data in data_request:
+            input_data.append(data.clocationinfo)
+        result_list = predict(input_data)
+        data_outputs = []
+        for result in result_list:
+            data_outputs.append(core_pb2.Dataoutput(clocationinfo=result))
+        response = core_pb2.AlertResponse(dataresponse=data_outputs, predict_resutlt="result")
+        return response
 
 def serve():
-    port = '8000'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    data_pb2_grpc.add_DataServiceServicer_to_server(DataService(), server)
-    server.add_insecure_port('[::]:'+port)
+    core_pb2_grpc.add_coreServicer_to_server(CoreServicer(), server)
+    server.add_insecure_port('[::]:8080')
     server.start()
-    print("server started, listening on port 8000")
     server.wait_for_termination()
 
 if __name__ == '__main__':
