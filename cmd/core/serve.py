8321c1c5ed766857228b810edb036d72ceca0bdc
张庙松 <1875912440@qq.com>
2023-04-15 19:47:46 +0800

    finish:finish the core function

--- cmd/core/serve.py
+++ cmd/core/serve.py
@@ -1,50 +1,85 @@
+import pickle
+
 import grpc
-import core_pb2_grpc
-import core_pb2
-import CorrelationAnalysis
-import predict
-import Train
 from concurrent import futures
-import grpc
+import core_pb2
+import core_pb2_grpc
+import analyse
+import train
+def predict(input_data):
+    # 从文件中加载模型
+    print("开始预测")
+    with open('model.pkl', 'rb') as f:
+        model = pickle.load(f)
 
+    # 从文件中读取变量
+    with open("locs.pkl", "rb") as f:
+        locs = pickle.load(f)
+    with open("locToIndexOflocs.pkl", "rb") as f:
+        locToIndexOflocs = pickle.load(f)
+    with open("train.pkl", "rb") as f:
+        train = pickle.load(f)
+
+    loc_index = [0] * len(input_data)
+    for i in range(len(input_data)):
+        loc_index[i] = locToIndexOflocs[input_data[i]]
+    Seqs = [loc_index]
+    p = model.predict(train, Seqs, 5, 8)
+    result = ([locs[i] for i in itemset] for itemset in p)
+    result_list = list(result)
+    count = 0
+
+    for i in range(len(result_list)):
+        for j in range(len(result_list[i])):
+            count += 1
+    return result_list
 class CoreServicer(core_pb2_grpc.coreServicer):
-    # 实现Train方法
+    # 实现 Analyse RPC
+    def Analyse(self, request, context):
+        # 处理请求数据
+        file_path = request.file_path
+        # 分析数据
+        all_account,level0,level1,level2,level3 = analyse.Analyse(file_path)
+
+        # 返回响应
+        return core_pb2.AnalyseResponse(
+            all_account=all_account,
+            level0=level0,
+            level1=level1,
+            level2=level2,
+            level3=level3,
+        )
     def Train(self, request, context):
-        # 进行训练，并返回结果
-        data = request.file_content
-        all_account, level0, level1, level2, level3 = Train(data)
-        response = core_pb2.TrainResponse(all_account=all_account, level0=level0, level1=level1, level2=level2, level3=level3)
-        return response
+        file_path = request.file_path
+        state = train.Train(file_path)
+        return core_pb2.TrainResponse(success=state)
 
-    # 实现CorrelationAnalysis方法
-    def CorrelationAnalysis(self, request, context):
-        # 进行相关性分析，并返回结果
-        data = request.file_content
-        state = CorrelationAnalysis(data)
-        response = core_pb2.CorrelationAnalysisResponse(success=state, model_name="model", model_content=b"content", train_name="train", train_content=b"content", loc_name="loc", loc_content=b"content", locToIndexOflocs_name="locToIndexOflocs", locToIndexOflocs_content=b"content")
-        return response
-
-    # 实现Alert方法
     def Alert(self, request, context):
-        # 进行预测，并返回结果
-        data_request = request.datarequest
-        input_data = []
-        for data in data_request:
-            input_data.append(data.clocationinfo)
-        result_list = predict(input_data)
-        data_outputs = []
-        for result in result_list:
-            data_outputs.append(core_pb2.Dataoutput(clocationinfo=result))
-        response = core_pb2.AlertResponse(dataresponse=data_outputs, predict_resutlt="result")
-        return response
+        datarequest = request.datarequest
+        print(datarequest)
+        # 进行预测
+        result_list = predict([data.clocationinfo for data in datarequest])
+        dataoutput_list = []
+        for result in result_list[0]:
+            dataoutput = core_pb2.Dataoutput(clocationinfo=result)
+            dataoutput_list.append(dataoutput)
+        print("一共产生了" + str(len(result_list[0])) + "条关联告警")
+        # 返回响应
+        return core_pb2.AlertResponse(dataresponse=dataoutput_list)
+
 
 def serve():
-    port = '8080'
+    port = '8000'
+    # 创建服务器
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    # 注册服务
     core_pb2_grpc.add_coreServicer_to_server(CoreServicer(), server)
+    # 监听端口
     server.add_insecure_port('[::]:'+port)
+    # 启动服务器
     server.start()
-    print("server started, listening on port 8080")
+    print("Server started. Listening on port 8000.")
+    # 阻塞线程
     server.wait_for_termination()
 
 if __name__ == '__main__':
