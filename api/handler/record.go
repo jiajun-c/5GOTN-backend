7e434c84f1387c301c97a8688517aaa7e632d359
jiajunCheng <chengjiajun20@gmail.com>
2023-04-19 21:31:16 +0800

    stash the commit

--- api/handler/record.go
+++ api/handler/record.go
@@ -18,15 +18,14 @@ func GetAllRecords(ctx *gin.Context) {
 	}
 	resps := make([]*dal.RecordResp, 0)
 	for _, record := range records {
-		wt, wte := record.WarningTime.Format("2006/01/02 15:04"), record.WarningTimeEnd.Format("2006/01/02 15:04")
+		wt := record.WarningTime.Format("2006/01/02 15:04")
 		resp := &dal.RecordResp{
-			WarningId:      record.WarningId,
-			DeviceID:       record.DeviceID,
-			DeviceAddress:  record.DeviceAddress,
-			DevicePort:     record.DevicePort,
-			WarningTime:    wt,
-			WarningTimeEnd: wte,
-			WarningCode:    record.WarningCode,
+			WarningId:     record.WarningId,
+			DeviceID:      record.DeviceID,
+			DeviceAddress: record.DeviceAddress,
+			DevicePort:    record.DevicePort,
+			WarningTime:   wt,
+			WarningCode:   record.WarningCode,
 		}
 		resps = append(resps, resp)
 	}
@@ -49,14 +48,14 @@ func DeleteRecord(ctx *gin.Context) {
 }
 
 func FilterRecord(ctx *gin.Context) {
-	device_id := ctx.Query("device_id")
-	did, _ := strconv.Atoi(device_id)
-	device_address := ctx.Query("device_address")
-	warning_time_begin := ctx.Query("warning_time_begin")
-	warning_time_end := ctx.Query("warning_time_end")
-	start, _ := time.Parse("2006/01/02 15:04", warning_time_begin)
-	end, _ := time.Parse("2006/01/02 15:04", warning_time_end)
-	records, err := crud.SelectRecords(start, end, int64(did), device_address)
+	deviceId := ctx.Query("device_id")
+	did, _ := strconv.Atoi(deviceId)
+	deviceAddress := ctx.Query("device_address")
+	warningTimeBegin := ctx.Query("warning_time_begin")
+	warningTimeEnd := ctx.Query("warning_time_end")
+	start, _ := time.Parse("2006/01/02 15:04", warningTimeBegin)
+	end, _ := time.Parse("2006/01/02 15:04", warningTimeEnd)
+	records, err := crud.SelectRecords(start, end, int64(did), deviceAddress)
 	if err != nil {
 		ctx.JSON(200, gin.H{"error": err.Error()})
 		return
@@ -65,26 +64,26 @@ func FilterRecord(ctx *gin.Context) {
 }
 
 func InsertRecord(ctx *gin.Context) {
-	warning_id := ctx.PostForm("warning_id")
-	device_id := ctx.PostForm("device_id")
-	device_port := ctx.PostForm("device_port")
-	device_address := ctx.PostForm("device_address")
-	warning_time := ctx.PostForm("warning_time")
-	warning_time_end := ctx.PostForm("warning_time_end")
-	warning_code := ctx.PostForm("warning_code")
-	wid, _ := strconv.Atoi(warning_id)
-	did, _ := strconv.Atoi(device_id)
-	wtime, _ := time.Parse("2006/01/02 15:04", warning_time)
-	wtime_end, _ := time.Parse("2006/01/02 15:04", warning_time_end)
-	wcode, _ := strconv.Atoi(warning_code)
+	warningId := ctx.PostForm("warning_id")
+	deviceId := ctx.PostForm("device_id")
+	devicePort := ctx.PostForm("device_port")
+	deviceAddress := ctx.PostForm("device_address")
+	warningTime := ctx.PostForm("warning_time")
+	warningLevel := ctx.PostForm("warning_level")
+	warningCode := ctx.PostForm("warning_code")
+	wid, _ := strconv.Atoi(warningId)
+	did, _ := strconv.Atoi(deviceId)
+	wtime, _ := time.Parse("2006/01/02 15:04", warningTime)
+	wcode, _ := strconv.Atoi(warningCode)
+	wlevel, _ := strconv.Atoi(warningLevel)
 	crud.InsertRecord(&dal.Record{
-		WarningId:      int64(wid),
-		DeviceID:       int64(did),
-		DeviceAddress:  device_address,
-		DevicePort:     device_port,
-		WarningTime:    wtime,
-		WarningTimeEnd: wtime_end,
-		WarningCode:    int64(wcode),
+		WarningId:     int64(wid),
+		DeviceID:      int64(did),
+		DeviceAddress: deviceAddress,
+		DevicePort:    devicePort,
+		WarningTime:   wtime,
+		WarningCode:   int64(wcode),
+		WarningLevel:  int64(wlevel),
 	})
 	ctx.JSON(200, gin.H{
 		"message": "success",
--- dal/record.go
+++ dal/record.go
@@ -3,13 +3,13 @@ package dal
 import "time"
 
 type Record struct {
-	WarningId      int64     `json:"warning_id" xorm:"pk"`
-	DeviceID       int64     `json:"device_id" xorm:"device_id"`
-	DeviceAddress  string    `json:"device_address"`
-	DevicePort     string    `json:"device_port"`
-	WarningTime    time.Time `json:"warning_time"`
-	WarningTimeEnd time.Time `json:"warning_time_end"`
-	WarningCode    int64     `json:"warning_code"`
+	WarningId     int64     `json:"warning_id" xorm:"pk"`
+	DeviceID      int64     `json:"device_id" xorm:"device_id"`
+	DeviceAddress string    `json:"device_address"`
+	DevicePort    string    `json:"device_port"`
+	WarningTime   time.Time `json:"warning_time"`
+	WarningCode   int64     `json:"warning_code"`
+	WarningLevel  int64     `json:"warning_level"`
 }
 type RecordResp struct {
 	WarningId      int64  `json:"warning_id"`
